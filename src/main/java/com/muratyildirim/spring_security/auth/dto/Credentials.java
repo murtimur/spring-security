b77ab1aa961d0d7570352871a54a36baaa1a72ae
murat-yildirim@hotmail.com.tr <murat-yildirim@hotmail.com.tr>
2025-07-22 14:28:34 +0300

    update

--- src/main/java/com/muratyildirim/spring_security/auth/dto/Credentials.java
+++ src/main/java/com/muratyildirim/spring_security/auth/dto/Credentials.java
@@ -1,7 +1,5 @@
 package com.muratyildirim.spring_security.auth.dto;
 
-import jakarta.validation.constraints.NotBlank;
-
-public record Credentials(@NotBlank String username, @NotBlank String password) {
+public record Credentials(String username, String password) {
 
 }
